4430a020608df5bc6d8803e5cb4186228563c7ff
Tony Vo <tony@audition.cat>
2025-03-04 09:35:57 -0700

    updated material ui and headshot. added grid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import headshot from './headshot.jpg';
 import './App.css';
-import Button from '@mui/material/Button';
+import { Grid } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img width='300px' style={{ borderRadius: '50%' }} src={headshot} className="headshot" alt="headshot" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained">Hello World Button!</Button>
+        <h1>Hi, I'm Tony</h1>
+        <Grid container spacing={2} style={{padding: '2em'}}>
+          <Grid item xs={4}>
+            <img width='300px' style={{ borderRadius: '50%' }} src={headshot} className="headshot" alt="headshot" />
+          </Grid>
+          <Grid item xs={8}>
+            <div style={{textAlign: 'left'}}>
+              <h3>I’m a software engineer that loves collaboration and building tools that benefit my community. I’m passionate about the intersection of technology and the arts.</h3>
+              <p style={{fontSize: '18px'}}>I spent the last decade in New York City working professionally in the performing arts as a theater/television actor and musician. I got interested in web design after helping other artists with their portfolios, and eventually ventured into programming after founding a start up called Audition Cat to help performers keep track of their networks and auditions. We're currently working on building our public Beta.</p>
+            </div>
+          </Grid>
+        </Grid>
+
       </header>
     </div>
   );
